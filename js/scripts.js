fa013d2939692163a98deb92ee46df73e097f655
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-06-27 21:00:23 +0200

    ADD text img and infinite cicle

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ const images = [
 ];
 
 const imgContainer = document.querySelector('.img-container');
-
+const textContainer = document.querySelector('.text-container');
 
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
@@ -33,22 +33,42 @@ const next = document.querySelector('.next');
 for (let i = 0; i < images.length; i++){
     const img = document.createElement('img');
     img.src = images[i].image;
+    
+    const textDiv = document.createElement('p');
+
+    textDiv.innerHTML = images[i].title + '<br>' + images[i].text;
+
     imgContainer.append(img);
+    textContainer.append(textDiv);
+
     if(i == 0){
         img.classList.add('active');
+        textDiv.classList.add('active');
+        textDiv.classList.add('text');
     }
 }
 
 let currentImg = 0;
+
 const allImg = document.querySelectorAll('img');
+const allText = document.querySelectorAll('p');
 
 
 prev.addEventListener('click',
     function(){
         if(currentImg > 0){
             allImg[currentImg].classList.remove('active');
+            allText[currentImg].classList.remove('active');
             currentImg--;
             allImg[currentImg].classList.add('active');
+            allText[currentImg].classList.add('active');
+        }
+        else if(currentImg == 0){
+            allImg[currentImg].classList.remove('active');
+            allText[currentImg].classList.remove('active');
+            currentImg = allImg.length - 1;
+            allImg[currentImg].classList.add('active');
+            allText[currentImg].classList.add('active');
         }
     
     }
@@ -58,8 +78,18 @@ next.addEventListener('click',
     function(){
         if(currentImg < allImg.length - 1){
         allImg[currentImg].classList.remove('active');
+        allText[currentImg].classList.remove('active');
         currentImg++;
         allImg[currentImg].classList.add('active');
+        allText[currentImg].classList.add('active');
+        }
+        else if(currentImg == allImg.length - 1){
+        
+        allImg[currentImg].classList.remove('active');
+        allText[currentImg].classList.remove('active');
+        currentImg = 0;
+        allImg[currentImg].classList.add('active');
+        allText[currentImg].classList.add('active');   
         }
 
     }
