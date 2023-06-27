49880b6a923a5e1802a3cb9f229512d0e3d82ea2
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-06-27 16:35:32 +0200

    ADD cicle for and addEventListener

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+const imgContainer = document.querySelector('.img-container');
+
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+
+
+for (let i = 0; i < images.length; i++){
+    const img = document.createElement('img');
+    img.src = images[i].image;
+    imgContainer.append(img);
+    if(i == 0){
+        img.classList.add('active');
+    }
+}
+
+let currentImg = 0;
+const allImg = document.querySelectorAll('img');
+
+
+prev.addEventListener('click',
+    function(){
+        if(currentImg > 0){
+            allImg[currentImg].classList.remove('active');
+            currentImg--;
+            allImg[currentImg].classList.add('active');
+        }
+    
+    }
+)
+
+next.addEventListener('click',
+    function(){
+        if(currentImg < allImg.length - 1){
+        allImg[currentImg].classList.remove('active');
+        currentImg++;
+        allImg[currentImg].classList.add('active');
+        }
+
+    }
+)
